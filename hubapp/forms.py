27e9db7330158fa0dd3d6b388627b9b04e8c5c7d
Haimin Ku <ku26@purdue.edu>
2021-12-05 13:04:15 -0500

    password confirmation error

--- hubapp/forms.py
+++ hubapp/forms.py
@@ -32,14 +32,6 @@ class UserForm(UserCreationForm):
         model = CustomUser
         fields = ['username', 'password1', 'password2',  'first_name', 'last_name', 'email', 'nickname', 'gender']
 
-    def clean_password1(self):
-        password1 = self.cleaned_data.get('password1')
-        password2 = self.cleaned_data.get('password2')
-        if password1 != password2:
-            raise forms.ValidationError('Password confirmation does not match.')
-        else:
-            return password1
-
     def clean_email(self):
         email = self.cleaned_data.get('email')
         duplicate_email = CustomUser.objects.filter(email=email)
